ef0958122f0faa9dd8c21703198a81b640b2042c
tpAilu <40452319+tpAilu@users.noreply.github.com>
2018-12-22 20:50:56 +0100

    Add project page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'projects'
+   }); 
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
